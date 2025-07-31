fd190cc4766fa9ebfec2ceeb33900d991ffeedcb
Praveen Thathsara <praveenstudy823@gmail.com>
2025-07-31 15:30:35 +0530

    create controllers

---
+++ backend/controllers/user.js
@@ -0,0 +1,14 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User created" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
