98544b542382158a0dd01a2de50fc9cf875bc73f
Praveen Thathsara <praveenstudy823@gmail.com>
2025-07-31 15:27:49 +0530

    set mongo URI

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log("Server started on port 5000"));
+  })
+  .catch((err) => console.error(err));
