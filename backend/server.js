9579313e8719a83c86e58ac4f64e819f8a268169
Praveen Thathsara <praveenstudy823@gmail.com>
2025-07-31 15:46:06 +0530

    Update server and env

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,31 @@
+// server.js
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import userRoutes from "./routes/user.js";
 
 dotenv.config();
+
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/users", userRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(5000, () => console.log("Server started on port 5000"));
-  })
-  .catch((err) => console.error(err));
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
+  });
+})
+.catch((err) => console.error("MongoDB connection error:", err));
